5e34a3acca8cbb9fd1a9aa2194cb8e05803f42ee
Dominik Prokop <dominik.prokop@grafana.com>
2021-10-21 17:36:34 +0200

    Macros step 1

--- pkg/bigquery/macros.go
+++ pkg/bigquery/macros.go
@@ -1,10 +1,76 @@
 package bigquery
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend/gtime"
 	"github.com/grafana/sqlds/v2"
 )
 
-var macros = map[string]sqlds.MacroFunc{}
+func macroTable(query *sqlds.Query, args []string) (string, error) {
+	var connArgs ConnectionArgs
+	err := json.Unmarshal(query.ConnectionArgs, &connArgs)
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("`%s.%s.%s`", connArgs.Project, connArgs.Dataset, connArgs.Table), nil
+}
+
+// Example:
+//   $__millisTimeFrom(time) => "time >= '1572480000000'"
+func macroMillisTimeFrom(query *sqlds.Query, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("%w: expected 1 argument, received %d", errors.New("unexpected number of arguments"), len(args))
+	}
+
+	t := int64(time.Nanosecond) * query.TimeRange.From.UTC().UnixNano() / int64(time.Millisecond)
+	return fmt.Sprintf("%s >= '%d'", args[0], t), nil
+}
+
+// Example:
+// $__millisTimeTo(time) => "time <= '1572480000000'"
+func macroMillisTimeTo(query *sqlds.Query, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("%w: expected 1 argument, received %d", errors.New("unexpected number of arguments"), len(args))
+	}
+
+	t := int64(time.Nanosecond) * query.TimeRange.To.UTC().UnixNano() / int64(time.Millisecond)
+	return fmt.Sprintf("%s <= '%d'", args[0], t), nil
+}
+
+func macroTimeGroup(query *sqlds.Query, args []string) (string, error) {
+	if len(args) < 2 {
+		return "", fmt.Errorf("%w: expected 2 arguments, received %d", errors.New("macro $__timeGroup needs time column and interval"), len(args))
+	}
+
+	interval, err := gtime.ParseInterval(strings.Trim(args[1], `'`))
+
+	if err != nil {
+		return "", fmt.Errorf("error parsing interval %v", args[1])
+
+	}
+	timeVar := args[0]
+
+	// when month interval
+	if interval == 2678400000000000 {
+		return fmt.Sprintf("TIMESTAMP((PARSE_DATE(\"%%Y-%%m-%%d\",CONCAT( CAST((EXTRACT(YEAR FROM `%s`)) AS STRING),'-',CAST((EXTRACT(MONTH FROM `%s`)) AS STRING),'-','01'))))", timeVar, timeVar), nil
+	}
+
+	return fmt.Sprintf("TIMESTAMP_SECONDS(DIV(UNIX_SECONDS(`%s`), %v) * %v)", timeVar, interval.Seconds(), interval.Seconds()), nil
+}
+
+var macros = map[string]sqlds.MacroFunc{
+	"table":          macroTable,
+	"timeGroup":      macroTimeGroup,
+	"millisTimeFrom": macroMillisTimeFrom,
+	"millisTimeTo":   macroMillisTimeTo,
+}
 
 func (s *BigQueryDatasource) Macros() sqlds.Macros {
 	return macros
