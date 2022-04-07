d87a58dd46225c380557548e337119a276225b1f
Zoltán Bedi <zoltan.bedi@gmail.com>
2022-04-07 11:56:30 +0200

    Data type: Support GEOGRAPHY

--- pkg/bigquery/driver/utils_test.go
+++ pkg/bigquery/driver/utils_test.go
@@ -129,6 +129,14 @@ func Test_ConvertColumnValue(t *testing.T) {
 			expectedValue: "01:01:01",
 		},
 		{
+			name:          "GEOGRAPHY",
+			value:         bigquery.Value(bigquery.NullGeography{GeographyVal: "POINT(1.0 1.0)", Valid: true}),
+			columnType:    "GEOGRAPHY",
+			schema:        &bigquery.FieldSchema{Type: "GEOGRAPHY"},
+			expectedType:  "string",
+			expectedValue: "POINT(1.0 1.0)",
+		},
+		{
 			name: "RECORD",
 			value: bigquery.Value([]bigquery.Value{
 				bigquery.Value(int64(1)),
