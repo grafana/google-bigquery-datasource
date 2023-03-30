d172462f0c52d5ac5162965fee33a77781e89483
Zoltán Bedi <zoltan.bedi@gmail.com>
2023-03-30 09:23:55 +0200

    Use types from sdk

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-import { DataQuery, DataSourceJsonData, TimeRange } from '@grafana/data';
+import { DataQuery, TimeRange } from '@grafana/data';
+import { DataSourceOptions, DataSourceSecureJsonData } from '@grafana/google-sdk';
 import { EditorMode } from '@grafana/experimental';
 import { BigQueryAPI } from 'api';
 import {
@@ -21,20 +22,13 @@ export interface QueryRowFilter {
   preview: boolean;
 }
 
-export interface BigQueryOptions extends DataSourceJsonData {
-  authenticationType: string;
+export interface BigQueryOptions extends DataSourceOptions {
   flatRateProject?: string;
   processingLocation?: string;
   queryPriority?: QueryPriority;
-  tokenUri?: string;
-  clientEmail?: string;
-  defaultProject?: string;
-  privateKeyPath?: string;
 }
 
-export interface BigQuerySecureJsonData {
-  privateKey?: string;
-}
+export interface BigQuerySecureJsonData extends DataSourceSecureJsonData {}
 
 export enum GroupType {
   Time = 'time',
