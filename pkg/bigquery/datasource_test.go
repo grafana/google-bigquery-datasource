1dc08ad647327dae0d8db327181c69b485c0e3db
Will Browne <will.browne@grafana.com>
2023-06-29 15:46:03 +0100

    update sdk

--- pkg/bigquery/datasource_test.go
+++ pkg/bigquery/datasource_test.go
@@ -4,27 +4,20 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"io"
 	"testing"
 
 	bq "cloud.google.com/go/bigquery"
+	"github.com/grafana/grafana-bigquery-datasource/pkg/bigquery/api"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
-	"github.com/grafana/grafana-plugin-sdk-go/genproto/pluginv2"
+	"github.com/grafana/grafana-plugin-sdk-go/experimental/datasourcetest"
 	"github.com/grafana/sqlds/v2"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/api/cloudresourcemanager/v3"
 	"google.golang.org/api/option"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
-
-	"github.com/grafana/grafana-bigquery-datasource/pkg/bigquery/api"
 )
 
 func RunConnection(ds *BigQueryDatasource, connectionArgs json.RawMessage) (*sql.DB, error) {
@@ -255,59 +248,11 @@ func TestBigQueryMultiTenancy(t *testing.T) {
 		return i, err
 	})
 
-	instanceProvider := datasource.NewInstanceProvider(factory)
-	instanceMgr := instancemgmt.New(instanceProvider)
-
-	go func() {
-		err := backend.StandaloneServe(backend.ServeOpts{
-			QueryDataHandler: backend.QueryDataHandlerFunc(
-				func(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-					h, err := instanceMgr.Get(ctx, req.PluginContext)
-					if err != nil {
-						return nil, err
-					}
-					if ds, ok := h.(backend.QueryDataHandler); ok {
-						return ds.QueryData(ctx, req)
-					}
-					return nil, status.Error(codes.Unimplemented, "unimplemented")
-				}),
-			CallResourceHandler: backend.CallResourceHandlerFunc(
-				func(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-					h, err := instanceMgr.Get(ctx, req.PluginContext)
-					if err != nil {
-						return err
-					}
-					if ds, ok := h.(backend.CallResourceHandler); ok {
-						return ds.CallResource(ctx, req, sender)
-					}
-					return status.Error(codes.Unimplemented, "unimplemented")
-				}),
-			CheckHealthHandler: backend.CheckHealthHandlerFunc(
-				func(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-					h, err := instanceMgr.Get(ctx, req.PluginContext)
-					if err != nil {
-						return nil, err
-					}
-					if ds, ok := h.(backend.CheckHealthHandler); ok {
-						return ds.CheckHealth(ctx, req)
-					}
-					return nil, status.Error(codes.Unimplemented, "unimplemented")
-				}),
-		}, addr)
-		require.NoError(t, err)
-	}()
-
-	// TODO when plugin SDK is updated
-	//go func() {
-	//	err := datasource.ManageForTest(factory, datasource.ManageTestOpts{Address: addr, Dir: t.TempDir()})
-	//	require.NoError(t, err)
-	//}()
-
-	pc, shutdown, err := newPluginClient(addr)
+	tp, err := datasourcetest.Manage(factory, datasourcetest.ManageOpts{Address: addr})
 	require.NoError(t, err)
 	defer func() {
-		err = shutdown()
-		require.NoError(t, err)
+		err = tp.Shutdown()
+		t.Log("plugin shutdown error", err)
 	}()
 
 	pCtx := backend.PluginContext{DataSourceInstanceSettings: &backend.DataSourceInstanceSettings{
@@ -325,55 +270,55 @@ func TestBigQueryMultiTenancy(t *testing.T) {
 		responseSender := newTestCallResourceResponseSender()
 		ctx := context.Background()
 
-		resp, err := pc.QueryData(ctx, qdr)
+		resp, err := tp.Client.QueryData(ctx, qdr)
 		require.NoError(t, err)
 		require.NotNil(t, resp)
 		require.Equal(t, 1, factoryInvocations)
 
-		err = pc.CallResource(ctx, crr, responseSender)
+		err = tp.Client.CallResource(ctx, crr, responseSender)
 		require.NoError(t, err)
 		require.Equal(t, 1, factoryInvocations)
 
 		t.Run("Request from tenant #1 creates new instance", func(t *testing.T) {
 			ctx = metadata.AppendToOutgoingContext(context.Background(), "tenantID", tenantID1)
-			resp, err = pc.QueryData(ctx, qdr)
+			resp, err = tp.Client.QueryData(ctx, qdr)
 			require.NoError(t, err)
 			require.NotNil(t, resp)
 			require.Equal(t, 2, factoryInvocations)
 
 			// subsequent requests from tenantID1 with same settings will reuse instance
-			resp, err = pc.QueryData(ctx, qdr)
+			resp, err = tp.Client.QueryData(ctx, qdr)
 			require.NoError(t, err)
 			require.NotNil(t, resp)
 			require.Equal(t, 2, factoryInvocations)
 
 			var chRes *backend.CheckHealthResult
-			chRes, err = pc.CheckHealth(ctx, chr)
+			chRes, err = tp.Client.CheckHealth(ctx, chr)
 			require.NoError(t, err)
 			require.NotNil(t, chRes)
 			require.Equal(t, 2, factoryInvocations)
 
 			t.Run("Request from tenant #2 creates new instance", func(t *testing.T) {
 				ctx = metadata.AppendToOutgoingContext(context.Background(), "tenantID", tenantID2)
-				resp, err = pc.QueryData(ctx, qdr)
+				resp, err = tp.Client.QueryData(ctx, qdr)
 				require.NoError(t, err)
 				require.NotNil(t, resp)
 				require.Equal(t, 3, factoryInvocations)
 
 				// subsequent requests from tenantID2 with same settings will reuse instance
-				err = pc.CallResource(ctx, crr, responseSender)
+				err = tp.Client.CallResource(ctx, crr, responseSender)
 				require.NoError(t, err)
 				require.Equal(t, 3, factoryInvocations)
 			})
 
 			// subsequent requests from tenantID1 with same settings will reuse instance
 			ctx = metadata.AppendToOutgoingContext(context.Background(), "tenantID", tenantID1)
-			resp, err = pc.QueryData(ctx, qdr)
+			resp, err = tp.Client.QueryData(ctx, qdr)
 			require.NoError(t, err)
 			require.NotNil(t, resp)
 			require.Equal(t, 3, factoryInvocations)
 
-			chRes, err = pc.CheckHealth(ctx, chr)
+			chRes, err = tp.Client.CheckHealth(ctx, chr)
 			require.NoError(t, err)
 			require.NotNil(t, chRes)
 			require.Equal(t, 3, factoryInvocations)
@@ -384,90 +329,6 @@ func TestBigQueryMultiTenancy(t *testing.T) {
 	require.NotEqual(t, instances[0], instances[1])
 	require.NotEqual(t, instances[0], instances[2])
 	require.NotEqual(t, instances[1], instances[2])
-}
-
-func newPluginClient(addr string) (*testPluginClient, shutdownFunc, error) {
-	c, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	if err != nil {
-		return nil, noShutdown, err
-	}
-
-	plugin := &testPluginClient{
-		diagnosticsClient: pluginv2.NewDiagnosticsClient(c),
-		dataClient:        pluginv2.NewDataClient(c),
-		resourceClient:    pluginv2.NewResourceClient(c),
-	}
-
-	return plugin, func() error {
-		return c.Close()
-	}, nil
-}
-
-type testPluginClient struct {
-	dataClient        pluginv2.DataClient
-	diagnosticsClient pluginv2.DiagnosticsClient
-	resourceClient    pluginv2.ResourceClient
-}
-
-type shutdownFunc func() error
-
-var noShutdown = shutdownFunc(func() error {
-	return nil
-})
-
-func (p *testPluginClient) QueryData(ctx context.Context, r *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	req := backend.ToProto().QueryDataRequest(r)
-
-	resp, err := p.dataClient.QueryData(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return backend.FromProto().QueryDataResponse(resp)
-}
-
-func (p *testPluginClient) CheckHealth(ctx context.Context, r *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	req := &pluginv2.CheckHealthRequest{
-		PluginContext: backend.ToProto().PluginContext(r.PluginContext),
-	}
-
-	resp, err := p.diagnosticsClient.CheckHealth(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return backend.FromProto().CheckHealthResponse(resp), nil
-}
-
-func (p *testPluginClient) CallResource(ctx context.Context, r *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
-	protoReq := backend.ToProto().CallResourceRequest(r)
-	protoStream, err := p.resourceClient.CallResource(ctx, protoReq)
-	if err != nil {
-		if status.Code(err) == codes.Unimplemented {
-			return errors.New("method not implemented")
-		}
-
-		return err
-	}
-
-	for {
-		protoResp, err := protoStream.Recv()
-		if err != nil {
-			if status.Code(err) == codes.Unimplemented {
-				return errors.New("method not implemented")
-			}
-
-			if errors.Is(err, io.EOF) {
-				return nil
-			}
-
-			return err
-		}
-
-		if err = sender.Send(backend.FromProto().CallResourceResponse(protoResp)); err != nil {
-			return err
-		}
-	}
 }
 
 type testCallResourceResponseSender struct{}
