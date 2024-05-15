4178831056ec46d229687fced078450cf70269a1
Gareth Dawson <gwdawson.work@gmail.com>
2024-05-15 14:02:37 +0100

    fix

--- src/components/query-editor-raw/QueryToolbox.tsx
+++ src/components/query-editor-raw/QueryToolbox.tsx
@@ -73,7 +73,7 @@ export function QueryToolbox({ showTools, onFormatCode, onExpand, isExpanded, ..
             {onExpand && (
               <IconButton
                 onClick={() => onExpand(!isExpanded)}
-                name={isExpanded ? 'compress-arrows' : ('expand-arrows-alt' as any)}
+                name={isExpanded ? 'angle-up' : 'angle-down'}
                 size="xs"
                 tooltip={isExpanded ? 'Collapse editor' : 'Expand editor'}
               />
